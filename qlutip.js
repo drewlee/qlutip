2d0ea07ac9ad11256f7c7d42768f7ab24db5d85e
Andrew Lee <starsthatfell@gmail.com>
2011-08-19 22:47:44 -0500

    adding files

---
+++ qlutip.js
@@ -0,0 +1,84 @@
+var QluTip = function(){
+    var $doc;
+    
+    function build(){
+        var shell = '<div id="qlutip_overlay" class="qlutip-outer ' + this.st.className + '" style="display:none;">'
+            + '<div class="qlutip-inner">'
+            + '<a class="qlutip-close" href="#"></a>'
+            + '<div class="qlutip-content">' + this.st.html + '</div>'
+            + '</div>'
+            + '<div class="qlutip-arrow"></div>';
+            
+        this.st.html = shell;
+    }
+    
+    function append(){
+        this.$ct = $(this.st.html);
+        
+        this.$ct.appendTo(document.body)
+            .fadeIn(this.st.speed)
+            .css({
+                top: getPositions.call(this, 'top'),
+                left: getPositions.call(this, 'left')
+            })
+            .bind('click', function(e){e.stopPropagation();})
+            .find('a.qlutip-close').bind('click', function(){
+                this.destroy();
+            });
+
+        this.st.callback(this.$ct);
+    }
+    
+    function getPositions(c){
+        var st = this.st, coord, o;
+        
+        if(c == 'top'){
+            o = this.$el.offset().top;
+            coord = st.yAlign == 'bottom' ? o - (this.$ct.outerHeight() - this.$el.outerHeight()) + st.yOffset : o + st.yOffset;
+        }else{
+            o = this.$el.offset().left;
+            coord = st.xAlign == 'right' ? o - this.$ct.outerWidth() + st.xOffset : o + st.xOffset;
+        }
+
+        return coord;
+    }
+    
+    function bindClickToDoc(){
+        if(!$doc){$doc = $(document);}
+        $doc.bind('click', this.destroy);
+    }
+    
+    return {
+        launch: function($el, opts){
+            this.st = {
+                html: '',
+                callback: function(){},
+                className: 'qlutip-default',
+                mode: 'default',
+                xAlign: 'left',
+                yAlign: 'top',
+                xOffset: 0,
+                yOffset: 0,
+                speed: 250
+            };
+            this.$el = $el;
+            
+            $.extend(this.st, opts || {});
+            
+            if($('#qlutip_overlay').length){
+                this.destroy();
+                if(this.st.mode == 'toggle'){return;}
+            }
+
+            build.call(this);
+            append.call(this);
+            bindClickToDoc.call(this);
+        },
+        
+        destroy: function(){ console.log('fire');
+            var _this = QluTip;
+            _this.$ct.fadeOut(_this.st.speed, function(){$(this).remove();});
+            $doc.unbind('click', _this.destroy);
+        }
+    };
+}();
